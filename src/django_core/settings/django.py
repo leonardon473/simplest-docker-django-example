5028d3cef2ab96a1e8b51426dff4c6adff8e9c72
Leonardo Ramírez <leonardon473@gmail.com>
2021-12-08 22:00:45 -0600

    changed: default django settings are added.

--- src/django_core/settings/django.py
+++ src/django_core/settings/django.py
@@ -1,30 +1,123 @@
 # Django Settings
 
 import os
-from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-# You must set the SECRET_KEY environment variable before running this project
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = True
+from ..envtools import getenv
 
-MAGIC_MESSAGE = os.environ.get("MAGIC_MESSAGE")
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
 
-ALLOWED_HOSTS = ["django", "localhost", "127.0.0.1"]
-INSTALLED_APPS = []
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = getenv("SECRET_KEY", default="###SECRET_KEY###")
+
+DEBUG = getenv("DEBUG", default=False, coalesce=bool)
+
+ALLOWED_HOSTS = ["*"]
+
+USE_X_FORWARDED_HOST = False
+SESSION_COOKIE_HTTPONLY = True
+
+SERVER_URL = os.getenv("SERVER_URL", default="*")
+
+
+APPEND_SLASH = False
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
 
 ROOT_URLCONF = "src.django_core.urls"
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = "src.django_core.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("POSTGRES_DB_NAME", default="postgres"),
+        "USER": getenv("POSTGRES_USER", default="postgres"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD", default="postgres"),
+        "HOST": getenv("POSTGRES_HOST", default="postgres"),
+        "PORT": 5432,
+        "CONN_MAX_AGE": 600,
+        "DISABLE_SERVER_SIDE_CURSORS": True,
+    },
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Chile/Continental"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "../collected_static")
+STATIC_URL = "/static/"

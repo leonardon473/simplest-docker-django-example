c7e140258b9e177e4f3f35aff8e9960872b58afa
Leonardo Ramírez <leonardon473@gmail.com>
2021-12-22 13:02:10 -0600

    removed: Unused settings are added.

--- src/django_core/settings/django.py
+++ src/django_core/settings/django.py
@@ -4,10 +4,6 @@ import os
 
 from ..envtools import getenv
 
-# import sentry_sdk
-# from sentry_sdk.integrations.django import DjangoIntegration
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,9 +15,6 @@ ALLOWED_HOSTS = ["*"]
 
 USE_X_FORWARDED_HOST = False
 SESSION_COOKIE_HTTPONLY = True
-
-SERVER_URL = os.getenv("SERVER_URL", default="*")
-
 
 APPEND_SLASH = False
 
